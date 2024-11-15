0966679f49b9430249611e0f5059893aa6d95e62
Yang Shanglin <ysl20@tsinghua.org.cn>
2024-11-15 11:25:22 +0800

    add basic navigator

--- src/unitree_nav2/unitree_nav2/navigation_to_pose.py
+++ src/unitree_nav2/unitree_nav2/navigation_to_pose.py
@@ -5,60 +5,67 @@ import rclpy
 from rclpy.node import Node
 from rclpy.duration import Duration
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped
-from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
-from custom_interface.msg import NavigationToPose,NavState
+from geometry_msgs.msg import Pose, PoseStamped
+from unitree_nav2.steve_basic_navigator import BasicNavigator
+from action_msgs.msg import GoalStatus
+from custom_interface.msg import NavState
 
 class NavToPoseNode(Node):
 
     def __init__(self, name):
         super().__init__(name)
-        self.sub = self.create_subscription(NavigationToPose, "nav_to_pose", self.navigationToPose_callback, 10)
+        self.sub = self.create_subscription(PoseStamped, "nav_to_pose", self.navigationToPose_callback, 10)
         self.pub = self.create_publisher(NavState,"nav_state",10)
+        self.navigator = BasicNavigator()
+        initial_pose = Pose()
+        initial_pose.orientation.w = 1.0
+        self.navigator.setInitialPose(initial_pose)
+        self.navigator.waitUntilNav2Active()
 
     def navigationToPose_callback(self,msg):
         #self.get_logger().info('Publishing: "%s"' % msg.data)
 
-        navigator = BasicNavigator()
-
-        goal_pose = PoseStamped()
-        goal_pose.header.frame_id = 'map'
-        goal_pose.header.stamp = navigator.get_clock().now().to_msg()
-        goal_pose.pose.position.x = msg.pose_x
-        goal_pose.pose.position.y = msg.pose_y
-        goal_pose.pose.position.z = msg.pose_z
-        goal_pose.pose.orientation.x = msg.quat_x
-        goal_pose.pose.orientation.y = msg.quat_y
-        goal_pose.pose.orientation.z = msg.quat_z
-        goal_pose.pose.orientation.w = msg.quat_w
-
-        navigator.goToPose(goal_pose)
+        assert isinstance(msg, PoseStamped)
+        # assert msg.header.frame_id == 'map'
+        goal_pose = msg
+        goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
+        self.navigator.goToPose(goal_pose)
 
         i = 0
-        while not navigator.isTaskComplete():
+        while not self.navigator.isTaskComplete():
+            #################################################
+            #
+            # Implement some code here for your application!
+            #
+            ################################################
+
+            # Do something with the feedback
             i = i + 1
-            feedback = navigator.getFeedback()
+            feedback = self.navigator.getFeedback()
                         
             if feedback and i % 5 == 0:
+                print('Estimated time of arrival: ' + '{0:.0f}'.format(
+                  Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9)
+                  + ' seconds.')
+                
                 if Duration.from_msg(feedback.navigation_time) > Duration(seconds=600.0):
-                    navigator.cancelTask()
+                    self.navigator.cancelTask()
 
         nav_state_msg = NavState()
-        nav_state_msg.id = msg.id
-
-        result = navigator.getResult()
-        if result == TaskResult.SUCCEEDED:
+        
+        result = self.navigator.getResult()
+        if result == GoalStatus.SUCCEEDED:
             print('Goal succeeded!')
-            nav_state_msg.state = "nav_true"
-        elif result == TaskResult.CANCELED:
+            nav_state_msg.state = "nav_succeed"
+        elif result == GoalStatus.CANCELED:
             print('Goal was canceled!')
             nav_state_msg.state = "nav_interrupt"
-        elif result == TaskResult.FAILED:
+        elif result == GoalStatus.FAILED:
             print('Goal failed!')
-            nav_state_msg.state = "nav_false"
+            nav_state_msg.state = "nav_failed"
         else:
             print('Goal has an invalid return status!')
-            nav_state_msg.state = "nav_false"
+            nav_state_msg.state = "nav_missed"
 
         self.pub.publish(nav_state_msg)
 
